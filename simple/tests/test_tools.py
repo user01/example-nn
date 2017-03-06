c44f8be14dfe105c081922f4281b33aca8ae33c8
Erik Langenborg <erik.langenborg@gmail.com>
2017-03-06 13:24:19 -0500

    Updated tools for more detailed values

--- simple/tests/test_tools.py
+++ simple/tests/test_tools.py
@@ -2,7 +2,7 @@
 
 import unittest
 import math
-from simple.tools import forward_pass_sum, sigmoid
+from simple.tools import linear_forward, sigmoid_forward
 
 
 class TestForwardPass(unittest.TestCase):
@@ -10,15 +10,15 @@ class TestForwardPass(unittest.TestCase):
 
     def test_basic(self):
         """Basic 1*2 + 1*3 = 5"""
-        self.assertEqual(5, forward_pass_sum([1], [2, 3]))
+        self.assertEqual(5, linear_forward([1], [2, 3]))
 
     def test_advanced(self):
         """Formula 1*2 + 2*3 = 8"""
-        self.assertEqual(8, forward_pass_sum([2], [2, 3]))
+        self.assertEqual(8, linear_forward([2], [2, 3]))
 
     def test_longer(self):
         """Formula 1*1 + 1*2 + 2*3 + 5*4 = 8"""
-        self.assertEqual(29, forward_pass_sum([1, 2, 5], [1, 2, 3, 4]))
+        self.assertEqual(29, linear_forward([1, 2, 5], [1, 2, 3, 4]))
 
 
 class TestSigmoid(unittest.TestCase):
@@ -26,12 +26,12 @@ class TestSigmoid(unittest.TestCase):
 
     def test_basic(self):
         """Basic Sigmoid"""
-        self.assertAlmostEqual(1 / (1 + math.exp(5)), sigmoid(5))
+        self.assertAlmostEqual(1 / (1 + math.exp(5)), sigmoid_forward(5))
 
     def test_many(self):
         """Many Sigmoid"""
         for i in range(-100, 100):
-            self.assertAlmostEqual(1 / (1 + math.exp(i / 10)), sigmoid(i / 10))
+            self.assertAlmostEqual(1 / (1 + math.exp(i / 10)), sigmoid_forward(i / 10))
 
 
 if __name__ == '__main__':
--- simple/tools.py
+++ simple/tools.py
@@ -2,16 +2,51 @@
 
 import math
 
-def forward_pass_sum(inputs, weights):
-    """Sum inputs and weight products"""
+
+def linear_forward(inputs, weights):
+    """netj; Sum inputs and weight products"""
+    return linear_forward_details(inputs, weights)['full_value']
+
+def linear_forward_details(inputs, weights):
+    """Detailed Sum inputs and weight products"""
     if len(inputs) + 1 != len(weights):
         raise Exception("Inputs and Weights size mismatch")
     inputs_with_bias = [1] + inputs
     inputs_and_weights = zip(inputs_with_bias, weights)
     inputs_by_weights = [p[0] * p[1] for p in inputs_and_weights]
     full_value = sum(inputs_by_weights)
-    return full_value
+    return {
+        'inputs_with_bias': inputs_with_bias,
+        'inputs_and_weights': inputs_and_weights,
+        'inputs_by_weights': inputs_by_weights,
+        'full_value': full_value
+    }
 
-def sigmoid(value):
+
+def linear_backward(inputs, error, weights, learning_rate):
+    """New weights. Updated based on inputs, weights, node error, and learning rate"""
+    return linear_backward_details(inputs, error, weights, learning_rate)['weights_updated']
+
+def linear_backward_details(inputs, error, weights, learning_rate):
+    """Detailed values based on inputs, weights, node error, and learning rate"""
+    inputs_with_bias = [1] + inputs
+    weights_delta = [learning_rate * error * i for i in inputs_with_bias]
+    weights_updated = [p[0] + p[1] for p in zip(weights, weights_delta)]
+    return {
+        'inputs': inputs,
+        'learning_rate': learning_rate,
+        'error': error,
+        'weights_delta': weights_delta,
+        'weights': weights,
+        'weights_updated': weights_updated
+    }
+
+
+def sigmoid_forward(value):
     """Sigmoid function"""
     return 1 / (1 + math.exp(value))
+
+
+def sigmoid_backward(value):
+    """Backwards Sigmoid function (gradient)"""
+    return value * (1 - value)
