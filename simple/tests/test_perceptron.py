24bb028f6a99c2abf4393f83e959264e422f0ac5
Erik <erik.langenborg@gmail.com>
2017-03-06 18:57:43 -0500

    Test forward and backpropagation of perceptron

--- simple/tests/test_perceptron.py
+++ simple/tests/test_perceptron.py
@@ -4,23 +4,40 @@ import unittest
 
 from simple.perceptron import Perceptron
 
+VALUE_INPUTS = [
+    # A  B
+    [1, 0],
+    [0, 1]
+]
+VALUE_OUTPUTS = [1, 0]
+
+
 class TestPerceptron(unittest.TestCase):
     """Perceptron tests"""
 
-    def test_basic(self):
-        """Basic Perceptron"""
+    def setUp(self):
+        self.perceptron_c = Perceptron([0.1, 0.1, 0.1], 'c', ['a', 'b'])
+        self.perceptron_d = Perceptron([0.1, 0.1], 'd', ['c'])
 
-        value_inputs = [
-            # A  B
-            [1, 0],
-            [0, 1]
-        ]
-        # learning_rate = 0.3
+    def test_forward(self):
+        """Test the forward pass"""
 
-        perceptron_c = Perceptron([0.1, 0.1, 0.1], 'c', ['a', 'b'])
+        perceptron_c_output = self.perceptron_c.forward(VALUE_INPUTS[0])
+        self.assertAlmostEqual(perceptron_c_output, 0.55, 2)
 
-        # perceptron_c_output = perceptron_c.forward(value_inputs[0])
-        # self.assertAlmostEqual(perceptron_c_output, 0.55)
+        perceptron_d_output = self.perceptron_d.forward([perceptron_c_output])
+        self.assertAlmostEqual(perceptron_d_output, 0.54, 2)
+
+    def test_backprop(self):
+        """Test the backprop pass"""
+
+        weighted_error_d = VALUE_OUTPUTS[0] - 0.54
+        perceptron_d_error = self.perceptron_d.backward(0.54, weighted_error_d)
+        self.assertAlmostEqual(perceptron_d_error, 0.11, 2)
+
+        weighted_error_c = self.perceptron_d.weights()[1] * perceptron_d_error
+        perceptron_c_error = self.perceptron_c.backward(0.54, weighted_error_c)
+        self.assertAlmostEqual(perceptron_c_error, 0.0028, 4)
 
 
 if __name__ == '__main__':
