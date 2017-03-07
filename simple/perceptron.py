3e90f79116b9d1c32806571ff26b5a6e032bc2d6
Erik <erik.langenborg@gmail.com>
2017-03-07 01:03:49 -0500

    Layer for perceptrons and tests

--- simple/perceptron.py
+++ simple/perceptron.py
@@ -1,6 +1,8 @@
 
 """Perceptions"""
 
+import math
+
 from .tools import linear_forward, sigmoid_forward
 from .tools import linear_backward, sigmoid_backward
 from .tools import linear_forward_verbose
@@ -17,6 +19,10 @@ class Perceptron():
     def weights(self):
         """Returns a copy of the current weights"""
         return self._weights[:]
+
+    def input_counts(self):
+        """Returns a copy of the current weights"""
+        return len(self._weights) - 1
 
     def forward(self, inputs):
         """Run an input through the Perceptron. Returns the output value"""
@@ -37,7 +43,7 @@ class Perceptron():
         return linear_prints + sigmoid_prints
 
     def backward(self, output_unit, weighted_error):
-        """Returns updated weights and """
+        """Returns updated weights"""
         unit_error = sigmoid_backward(output_unit) * weighted_error
         return unit_error
 
@@ -46,3 +52,46 @@ class Perceptron():
         weights_updated = linear_backward(
             inputs, unit_error, self._weights, learning_rate)
         return Perceptron(weights_updated, self._name, self._input_names)
+
+
+class PerceptronLayer():
+    """Container for multiple Perceptrons in a layer"""
+
+    def __init__(self, perceptrons, name):
+        self._input_size = perceptrons[0].input_counts()
+        self._name = name
+        self._perceptrons = perceptrons
+
+    def perceptrons(self):
+        """Copy of layer perceptrons"""
+        return self._perceptrons[:]
+
+    @staticmethod
+    def blank(input_size, output_size, name, input_names):
+        """Produce a Perception Layer based on sizes"""
+        input_size_with_bias = input_size + 1
+        weights = [1 / math.sqrt(input_size_with_bias)] * input_size_with_bias
+
+        perceptrons = [Perceptron(weights[:], '{0}.{1}'.format(name, i), input_names)
+                       for i in range(0, output_size)]
+        return PerceptronLayer(perceptrons, name)
+
+    def forward(self, inputs):
+        """Perform feed forward on all Perceptrons"""
+        if len(inputs) != self._input_size:
+            raise Exception("Incorrect number of inputs")
+
+        return [p.forward(inputs) for p in self._perceptrons]
+
+    def backward(self, outputs, weighted_errors):
+        """Find error for each Perceptron based on their output and weighted errors"""
+        return [perceptron.backward(output_value, weighted_error) for
+                perceptron, output_value, weighted_error in
+                zip(self._perceptrons, outputs, weighted_errors)]
+
+    def update_weights(self, inputs, unit_errors, learning_rate):
+        """Updated weights on perceptrons layer"""
+        perceptrons_new = [perceptron.update_weights(inputs, unit_error, learning_rate) for
+                           perceptron, unit_error in zip(self._perceptrons, unit_errors)]
+        return PerceptronLayer(perceptrons_new, self._name)
+
---
+++ simple/tests/test_perceptronlayer.py
@@ -0,0 +1,69 @@
+"""Test Perceptrons"""
+
+import unittest
+
+from simple.perceptron import Perceptron, PerceptronLayer
+
+
+class TestPerceptronLayer(unittest.TestCase):
+    """Perceptron Layer tests"""
+
+    def test_init(self):
+        """Test the constructing a layer"""
+        new_layer = PerceptronLayer(
+            [
+                Perceptron([1, 2], 'a', ['i']),
+                Perceptron([1, 3], 'b', ['i'])
+            ],
+            'layer1'
+        )
+        self.assertEqual(2, len(new_layer.perceptrons()))
+        self.assertEqual(1, new_layer.perceptrons()[0].input_counts())
+
+    def test_init_blank(self):
+        """Test the constructing a blank layer"""
+        new_layer = PerceptronLayer.blank(4, 2, 'layer1', ['a', 'b', 'c', 'd'])
+        self.assertEqual(2, len(new_layer.perceptrons()))
+        self.assertEqual(4, new_layer.perceptrons()[0].input_counts())
+
+    def test_forward(self):
+        """Test feeding forward through a layer"""
+        new_layer = PerceptronLayer.blank(4, 2, 'layer1', ['a', 'b', 'c', 'd'])
+        results_actual = new_layer.forward([1, 2, 1, 2])
+        results_expected = [0.958, 0.958]
+        for expected, actual in zip(results_expected, results_actual):
+            self.assertAlmostEqual(actual, expected, 2)
+
+    def test_backward(self):
+        """Test feeding backward through a layer"""
+        new_layer = PerceptronLayer.blank(4, 2, 'layer1', ['a', 'b', 'c', 'd'])
+        example_outputs = [0.958, 0.958]
+        example_errors = [0.05, 0.05]
+        unit_errors_actual = new_layer.backward(
+            example_outputs, example_errors)
+        unit_errors_expected = [0.002, 0.002]
+
+        for expected, actual in zip(unit_errors_expected, unit_errors_actual):
+            self.assertAlmostEqual(actual, expected, 2)
+
+    def test_update_weights(self):
+        """Test update_weights via a layer"""
+        layer = PerceptronLayer.blank(4, 2, 'layer1', ['a', 'b', 'c', 'd'])
+
+        example_inputs = [0.958, 0.958]
+        example_errors = [0.05, 0.05]
+        layer_updated = layer.update_weights(
+            example_inputs, example_errors, 0.05)
+
+        perceptrons_old = layer.perceptrons()
+        perceptrons_new = layer_updated.perceptrons()
+
+        self.assertEqual(len(perceptrons_new), len(perceptrons_old))
+
+        for perceptron_old, perceptron_new in zip(perceptrons_old, perceptrons_new):
+            for weight_old, weight_new in zip(perceptron_old.weights(), perceptron_new.weights()):
+                self.assertNotEqual(weight_old, weight_new)
+
+
+if __name__ == '__main__':
+    unittest.main()
