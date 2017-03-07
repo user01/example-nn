25491ab99f7abb22d03363b796b58ff923388f42
Erik <erik.langenborg@gmail.com>
2017-03-07 02:01:28 -0500

    Testing of layer backwards

--- simple/tests/test_perceptronnetwork.py
+++ simple/tests/test_perceptronnetwork.py
@@ -46,5 +46,24 @@ class TestPerceptronNetwork(unittest.TestCase):
         for expected, actual in zip(results_expected, results_actual):
             self.assertAlmostEqual(actual, expected, 2)
 
+    def test_backward(self):
+        """Test feeding forward"""
+        network = PerceptronNetwork(
+            [
+                PerceptronLayer.blank(4, 4, 'layer1', ['a', 'b', 'c', 'd']),
+                PerceptronLayer.blank(4, 2, 'layer2', ['a', 'b', 'c', 'd']),
+                PerceptronLayer.blank(2, 2, 'layer3', ['a', 'b', 'c', 'd'])
+            ]
+        )
+        _, layer_states = network.forward([1, 2, 1, 2])
+        truths = [1, 0.5]
+
+        unit_errors_actual = network.backward(layer_states, truths)
+        self.assertEqual(3, len(unit_errors_actual))
+        self.assertEqual(4, len(unit_errors_actual[0]))
+        self.assertEqual(2, len(unit_errors_actual[1]))
+        self.assertEqual(2, len(unit_errors_actual[2]))
+
+
 if __name__ == '__main__':
     unittest.main()
